27cf0653c0282c4a2067c6f282967a0720101fcf
Prabesh Sharma <prwshshrm@gmail.com>
2023-08-01 04:11:14 +0300

    Changes for deployment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import env from "dotenv";
 
 import "./styles/Styles.scss";
 
-env.config();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <App />
