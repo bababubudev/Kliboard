383b92827d1160188d7ba53c7c98ad8a9d70704a
Prabesh Sharma <51091892+bababubudev@users.noreply.github.com>
2024-07-02 18:27:37 +0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     mode: process.env.NODE_ENV,
-    base: "/Kliboard/"
+    base: "/Kliboard/",
     plugins: [
         react(),
     ],
